a50cac45ad9cfb94918900cd6bb0a4cd2bc437da
jojoaditya7-hub <jojoaditya7@gmail.com>
2026-05-29 09:59:11 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-orange-500 flex items-center justify-center">
+      <div className="bg-white p-10 rounded-3xl shadow-2xl">
+        <h1 className="text-4xl font-bold text-orange-500">TEST TAILWIND</h1>
+      </div>
+    </div>
+  );
+}
